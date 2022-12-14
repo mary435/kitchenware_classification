9a82c45faca79e6cc00d022e96193239d29bb498
Marilina Orihuela <mary.orihuela@gmail.com>
2022-12-14 13:53:52 -0300

    upload test

--- test.py
+++ test.py
@@ -2,13 +2,16 @@
 # coding: utf-8
 
 import requests
+import sys
 
-#url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
 
-url = 'https://oe6aug0jqj.execute-api.sa-east-1.amazonaws.com/Test/predict'
+#url = 'https://oe6aug0jqj.execute-api.sa-east-1.amazonaws.com/Test/predict'
 
-
-data = {'url': 'https://raw.githubusercontent.com/mary435/kitchenware_classification/main/images/6172.jpg'}
+if len(sys.argv)==2:
+    data = {'url': sys.argv[1]}
+else:
+    data = {'url': 'https://raw.githubusercontent.com/mary435/kitchenware_classification/main/images/6172.jpg'}
 
 result = requests.post(url, json=data).json()
 print(result)
